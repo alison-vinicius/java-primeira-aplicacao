713f6c232ae0e84b69c2f743c289f0e0382eca22
alison-vinicius <alison.soares@sou.ufac.br>
2024-07-10 11:59:16 -0500

    adicionando loops

---
+++ src/Loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double meidaAvAliacao = 0;
+        double nota;
+
+
+        for(int i= 1; i <5; i++){
+            System.out.println("oi");
+        }
+
+
+
+    }
+}
+
+
