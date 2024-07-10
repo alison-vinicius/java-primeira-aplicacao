e31c5ab2d48f7ce34fae5899d4c9afd80af31322
alison-vinicius <alison.soares@sou.ufac.br>
2024-07-10 12:01:03 -0500

    acrescentando codigos de leitura

--- src/Leitura.java
+++ src/Leitura.java
@@ -6,9 +6,20 @@ public class Leitura {
 
         System.out.println("digite seu filme favorito:");
         String filme = leitura.nextLine();
-        System.out.println(filme);
         System.out.println("qual o ano de lançamento?");
         int anoDeLancamento = leitura.nextInt();
+        System.out.println("qual a sua avaliação?");
+        double avaliacao = leitura.nextDouble();
+
+        String mensagem = """
+                filme: %s
+                ano de lançamento: %d
+                avaliação: %f
+                """.formatted(filme, anoDeLancamento, avaliacao);
+        System.out.println(mensagem);
+
+
+
 
     }
 }
